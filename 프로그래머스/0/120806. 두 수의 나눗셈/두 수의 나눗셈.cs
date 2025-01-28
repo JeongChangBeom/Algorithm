fa813bb3a361fa42c03ee387ba6a29d9a6da5d4b
ChangBeom <87409157+JeongChangBeom@users.noreply.github.com>
2025-01-28 15:56:04 +0900

    [level 0] Title: 두 수의 나눗셈, Time: 0.14 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120806. 두 수의 나눗셈/두 수의 나눗셈.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class Solution {
+    public int solution(int num1, int num2) {
+        int answer = 0;
+        
+        float temp = (float)num1/num2 * 1000;
+        
+        answer = (int)temp;
+        
+        return answer;
+    }
+}
