74b1d80e076e23a7da0aa97d65125dc92837b182
ChangBeom <87409157+JeongChangBeom@users.noreply.github.com>
2025-01-28 16:06:33 +0900

    [level 0] Title: 양꼬치, Time: 0.17 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120830. 양꼬치/양꼬치.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class Solution {
+    public int solution(int n, int k) {
+        int answer = 0;
+        
+        answer += (n*12000) + (k*2000) - ((n/10)*2000);
+        
+        
+        return answer;
+    }
+}
