3848f08128fd8fa90219a9af34c3dec200fd3291
ChangBeom <87409157+JeongChangBeom@users.noreply.github.com>
2025-01-30 18:36:12 +0900

    [level 0] Title: 배열 자르기, Time: 0.48 ms, Memory: 31.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120833. 배열 자르기/배열 자르기.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+
+public class Solution {
+    public List<int> solution(int[] numbers, int num1, int num2) {
+        List<int> answer = new List<int>();
+        
+        for(int i=num1;i<=num2;i++){
+            answer.Add(numbers[i]);
+        }
+        
+        return answer;
+    }
+}
