ea519f5f4ad4e439c012e01aada0d5a8559afab0
ChangBeom <87409157+JeongChangBeom@users.noreply.github.com>
2025-09-15 17:54:02 +0900

    [Bronze V] Title: 시험 성적, Time: 48 ms, Memory: 5472 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.cs
@@ -0,0 +1,28 @@
+class Program
+{
+    static void Main()
+    {
+        int score = int.Parse(Console.ReadLine());
+
+        if (score >= 90)
+        {
+            Console.WriteLine("A");
+        }
+        else if (score < 90 && score >= 80)
+        {
+            Console.WriteLine("B");
+        }
+        else if (score < 80 && score >= 70)
+        {
+            Console.WriteLine("C");
+        }
+        else if (score < 70 && score >= 60)
+        {
+            Console.WriteLine("D");
+        }
+        else
+        {
+            Console.WriteLine("F");
+        }
+    }
+}
