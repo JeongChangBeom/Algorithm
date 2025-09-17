38aa657cb2a8f1d56377b7483d54f43e5050e5a6
ChangBeom <87409157+JeongChangBeom@users.noreply.github.com>
2025-09-17 16:47:58 +0900

    [level 3] Title: 이중우선순위큐, Time: 1339.38 ms, Memory: 47.7 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.cs
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.cs
@@ -1,43 +1,13 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
 
 public class Solution {
     public int[] solution(string[] operations) {
-        List<int> list = new List<int>();
+        int[] answer = new int[] {};
         
-        foreach(var op in operations){
-            var input = op.Split();
-            
-            string cmd = input[0];
-            int num = int.Parse(input[1]);
-            
-            if(cmd == "I"){
-                list.Add(num);
-            }
-            else if(cmd == "D"){
-                if(list.Count == 0){
-                    continue;
-                }
-                else{
-                    list.Sort();
-                    
-                    if(num == 1){
-                        list.RemoveAt(list.Count-1);
-                    }
-                    else{
-                        list.RemoveAt(0);
-                    }
-                }
-            }
-        }
+        var maxpq = new PriorityQueue<int,int>(Comparer<int>.Create((a,b) => b.CompareTo(a)));
+        var minpq = new PriorityQueue<int,int>();
         
-        if(list.Count == 0){
-            return new int[] {0,0};
-        }
-        else{
-            list.Sort();
-            return new int[] {list[list.Count-1], list[0]};
-        }
+        return answer;
     }
 }
