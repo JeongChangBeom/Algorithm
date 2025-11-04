8b0038cf1766eee6bb64f3a985904f6979fd8c32
ChangBeom <87409157+JeongChangBeom@users.noreply.github.com>
2025-11-04 15:15:24 +0900

    [level 0] Title: 더 크게 합치기, Time: 3.32 ms, Memory: 31.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.cs
@@ -0,0 +1,19 @@
+using System;
+
+public class Solution {
+    public int solution(int a, int b) {
+        int answer = 0;
+        
+        int num_1 = int.Parse($"{a}{b}");
+        int num_2 = int.Parse($"{b}{a}");
+        
+        if(num_1 >= num_2){
+            answer = num_1;
+        }
+        else{
+            answer = num_2;
+        }
+        
+        return answer;
+    }
+}
