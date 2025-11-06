f263a3ac818b3fc8b9435870915860f4b59795fc
ChangBeom <87409157+JeongChangBeom@users.noreply.github.com>
2025-11-06 18:35:08 +0900

    [level 0] Title: 배열 비교하기, Time: 0.30 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181856. 배열 비교하기/배열 비교하기.cs
@@ -0,0 +1,35 @@
+using System;
+
+public class Solution {
+    public int solution(int[] arr1, int[] arr2) {
+        int answer = 0;
+        
+        if(arr1.Length > arr2.Length){
+            answer = 1;
+        }
+        else if(arr1.Length == arr2.Length){
+            int sum1 = 0;
+            int sum2 = 0;
+            
+            for(int i=0;i<arr1.Length;i++){
+                sum1 += arr1[i];
+                sum2 += arr2[i];
+            }
+            
+            if(sum1 > sum2){
+                answer = 1;
+            }
+            else if (sum1 == sum2){
+                answer = 0;
+            }
+            else{
+                answer = -1;
+            }
+        }
+        else{
+            answer = -1;
+        }
+        
+        return answer;
+    }
+}
