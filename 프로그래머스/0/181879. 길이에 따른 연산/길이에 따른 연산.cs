08fc7494c40bf076d96fef900af9d29c1203a6b1
ChangBeom <87409157+JeongChangBeom@users.noreply.github.com>
2025-11-07 15:02:24 +0900

    [level 0] Title: 길이에 따른 연산, Time: 1.28 ms, Memory: 31.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181879. 길이에 따른 연산/길이에 따른 연산.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Linq;
+
+public class Solution {
+    public int solution(int[] num_list) {
+        int answer = 0;
+        
+        if(num_list.Length > 10){
+            answer = num_list.Sum();
+        }
+        else{
+            answer = num_list.Aggregate(1, (a,b) => a*b);
+        }
+        
+        return answer;
+    }
+}
